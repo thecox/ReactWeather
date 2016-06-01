204144ae7fce05d3ec030978bcf4b8fed35060b1
Andrew Cox <andrew.cox@MacBook-Pro.local>
2016-06-01 17:22:36 -0600

    Styling for Weather page

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -2,9 +2,7 @@ var React = require('react');
 
 var WeatherMessage = ({temp, location}) => {
   return (
-    <div>
-      <p>It is {temp} degrees in {location}!</p>
-    </div>
+    <h3 className="text-center">It is {temp} degrees in {location}!</h3>
   );
 };
 
